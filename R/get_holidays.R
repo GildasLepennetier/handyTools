82e6c91c7e9b31d5397e5e2406cd76c9607fbb9c
Lepennetier <GildasLepennetier@users.noreply.github.com>
2024-09-18 11:08:03 +0200

    holidays: return better output using tibble and unnest

--- R/get_holidays.R
+++ R/get_holidays.R
@@ -7,6 +7,8 @@
 #' @param to_date end date
 #' @importFrom jsonlite fromJSON
 #' @importFrom dplyr select
+#' @importFrom tidyr unnest
+#' @importFrom tibble as_tibble
 #' @export
 #' @return data frame
 #' @examples
@@ -14,14 +16,16 @@
 
 get_holidays <- function(from_date = format(Sys.Date(), "%Y-01-01") ,
 												 to_date = format(Sys.Date(), "%Y-12-31"),
-												 countryIsoCode = "DE", languageIsoCode = "DE"){
+												 countryIsoCode = "DE", languageIsoCode = "EN"){
   url <- paste0("https://openholidaysapi.org/PublicHolidays",
                 "?countryIsoCode=",countryIsoCode,
                 "&languageIsoCode=",languageIsoCode,
                 "&validFrom=",from_date,
                 "&validTo=",to_date)
-  public_holidays <- fromJSON(url) %>%
-  	select("name", "type", "startDate", "endDate")
+  public_holidays <- jsonlite::fromJSON(url)
+  public_holidays <- public_holidays %>%
+  	tibble::as_tibble() %>%
+  	tidyr::unnest(cols = c("name", "subdivisions"))
 
   return(public_holidays)
 }
