77abbfa4c3f361f458715f33236b80726db06c0d
Lepennetier <GildasLepennetier@users.noreply.github.com>
2025-11-17 12:35:49 +0100

    correct error in ID

--- R/extract_json_v6.R
+++ R/extract_json_v6.R
@@ -16,11 +16,11 @@
 extract_json_v6 <- function (.x, .id, .col, relationship = NULL){
   .col = enquo(.col)
   .id = enquo(.id)
-  df1 <- map2_dfr(.x %>% pull({{.id}}), .x %>% pull({{.col}}), .f = function(id, json) {
+  df1 <- map2_dfr(.x %>% pull({{.id}}), .x %>% pull({{.col}}), .f = function(this_id, json) {
     this_json <- fromJSON(json, flatten = T)
     # if an id column exists, rename it
-    if ("id" %in% colnames(this_json)) {this_json <- this_json %>% rename(json_id = id)}
-    this_json$id = .id # force the creation of an ID for merge, use the input .id
+    if ("id" %in% colnames(this_json)) {this_json <- this_json %>% rename("json_id" = "id")}
+    this_json$id = this_id # force the creation of an ID for merge, use the input .id
     return(this_json)
   })
   df2 <- .x %>% as_tibble() %>% left_join(df1, by = join_by({{.id}} == .data$id), relationship = relationship)
