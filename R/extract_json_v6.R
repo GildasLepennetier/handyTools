8d1267f42f8f42033450da5f5975cb1c87281035
Lepennetier <gildas.lepennetier@hotmail.fr>
2026-07-09 11:49:55 +0200

    add auto remove from df

--- R/extract_json_v6.R
+++ R/extract_json_v6.R
@@ -4,6 +4,7 @@
 #' @param .id the ID column to use in case of line duplication
 #' @param .col column with json to extract
 #' @param relationship if some many-to-many are expected, use this option
+#' @param rm_col remove the column from the df returned since data is extracted
 #'
 #' @importFrom dplyr pull
 #' @importFrom dplyr as_tibble
@@ -16,7 +17,7 @@
 #' @importFrom dplyr join_by
 #' @return dataframe with extracted json columns#'
 #' @export
-extract_json_v6 <- function (.x, .id, .col, relationship = NULL){
+extract_json_v6 <- function (.x, .id, .col, relationship = NULL, rm_col = TRUE){
   .col = enquo(.col)
   .id = enquo(.id)
   df1 <- map2_dfr(.x %>% pull({{.id}}), .x %>% pull({{.col}}), .f = function(this_id, json) {
@@ -27,5 +28,8 @@ extract_json_v6 <- function (.x, .id, .col, relationship = NULL){
     return(this_json)
   })
   df2 <- .x %>% as_tibble() %>% left_join(df1, by = join_by({{.id}} == "id"), relationship = relationship)
+  if(rm_col) {
+    df2 <- df2 |> select(-{{.col}})
+  }
   return(df2)
 }
